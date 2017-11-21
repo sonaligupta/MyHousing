d0b4d7194a89d5097fea8b3daf226a3a46f4f5c8
rforravipatel <rforravipatel@gmail.com>
2017-11-20 19:52:27 -0500

    Dashboard added

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,7 +5,26 @@
     .module('core')
     .controller('HomeController', HomeController);
 
-  function HomeController() {
-    var vm = this;
-  }
-}());
+    HomeController.$inject = ['$scope', '$rootScope', 'Authentication', '$location', 'Notification'];
+    function HomeController($scope, $rootScope, Authentication,$location, Notification) {
+      var vm = this;
+      if(Authentication.user){
+        $rootScope.isUserLoggedIn = true;
+      }
+  
+      // Get an eventual error defined in the URL query string:
+      if ($location.search().err) {
+        Notification.error({ message: $location.search().err });
+      } 
+      // If user is signed in then redirect back home
+      if (!Authentication.user) {
+        $location.path('/');
+      }else{
+        $location.path('/settings/dashboard');
+      }
+      $scope.isSignInClicked = false;
+      $scope.signupClicked = function(){
+        $scope.isSignInClicked = true;
+      };
+    }
+  }());
